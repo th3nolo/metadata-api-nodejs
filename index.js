e10d1b89dfe326cdc0655166783a7665f1404705
Manuel Parra <th3nolo@gmail.com>
2022-08-10 21:06:02 -0400

    change the data of the NFT

--- index.js
+++ index.js
@@ -20,17 +20,18 @@ app.get('/', function(req, res) {
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
+  const nft = db[tokenId]
+  //const bdayParts = nft.birthday.split(' ')
+  //const day = parseInt(bdayParts[1])
+  //const month = parseInt(bdayParts[0])
+  
   const data = {
-    'name': person.name,
+    'name': nft.name,
     'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
+      'type': nft.type,
+      'stakeTime': nft.stakeTime,
+      'transparent': nft.transparent,    
+      'fruits': nft.fruits
     },
     'image': `${HOST}/images/${tokenId}.png`
   }
@@ -42,14 +43,14 @@ app.listen(app.get('port'), function() {
 })
 
 // returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
+//function zodiac(day, month) {
+//  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+//  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+//  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
+//}
 
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
+//function monthName(month) {
+//  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+//  ]
+//  return monthNames[month - 1]
+//}
--- src/database.js
+++ src/database.js
@@ -3,25 +3,29 @@ module.exports = {
     "name" : "trees - rewards",
     "type": "seed",
     "stakeTime": "1 day",
-    "transparent": "true"  
+    "transparent": "true",
+    "fruits": "false"  
   },
   "2": {
     "name" : "trees - rewards",
     "type": "sprout",
     "stakeTime": "5 days",
-    "transparent": "true"  
+    "transparent": "true",
+    "fruits": "false" 
   },
   "3": {
     "name" : "trees - rewards",
-    "type": "seedling ",
+    "type": "seedling",
     "stakeTime": "2 weeks",
-    "transparent": "true"  
+    "transparent": "true",
+    "fruits": "false"   
   },
   "4": {
     "name" : "trees - rewards",
     "type": "mature",
     "stakeTime": "3 months",
-    "transparent": "true"  
+    "transparent": "true",
+    "fruits": "false"  
   },
   "5": {
     "name" : "trees - rewards",
