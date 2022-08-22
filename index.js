fd0f54b0036ee564a86a063df4da40c59a9e2e0a
Manuel Parra <th3nolo@gmail.com>
2022-08-22 15:51:23 -0400

    added a way to refresh the metadata w/o hitting rate limits.. test

--- index.js
+++ index.js
@@ -204,6 +204,7 @@ app.get("/api/token/:token_id", async function (req, res) {
 
   const tokenId = parseInt(req.params.token_id).toString();  
   let stakeTimeArray = await ownerOf(tokenId);
+  setTimeout(refreshMetadata, 5000, tokenId)
   let _result = stakeTimeArray.result[stakeTimeArray.result.length - 1];
   //Block timestamp are in dateNowISO8601, you need to convert it using new Date, to get in epoch time, toISOString() converts the Date into a string in ISO 8601 format.
   //console.log(firstDeposit.toISOString(), dateNow.toISOString());
@@ -297,8 +298,7 @@ app.get("/api/token/:token_id", async function (req, res) {
     },
     image: `${HOST}/images/${level}.png`,
   };
-  res.send(data);
-  setTimeout(refreshMetadata, 5000, tokenId)
+  res.send(data);  
 });
 
 const startServer = async () => {
